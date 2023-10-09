09ec0495f983a1f289503781b3a5473752115b41
moises serrano <moycs777@gmail.com>
2023-10-09 12:14:56 -0300

    wip: login page

--- src/layouts/AppTopbar.vue
+++ src/layouts/AppTopbar.vue
@@ -1,68 +1,69 @@
 <script setup lang="ts">
-// import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
+import { useLayout } from '../layouts/composables/layout'
 // import { useLayout } from '@/layout/composables/layout'
 import { useRouter } from 'vue-router'
 
-// const { layoutConfig, onMenuToggle } = useLayout()
+const { layoutConfig, onMenuToggle } = useLayout()
 
-// const outsideClickListener = ref(null)
-// const topbarMenuActive = ref(false)
-// const router = useRouter()
+const outsideClickListener = ref(null)
+const topbarMenuActive = ref(false)
+const router = useRouter()
 
-// onMounted(() => {
-//   bindOutsideClickListener()
-// })
+onMounted(() => {
+  bindOutsideClickListener()
+})
 
-// onBeforeUnmount(() => {
-//   unbindOutsideClickListener()
-// })
+onBeforeUnmount(() => {
+  unbindOutsideClickListener()
+})
 
-// const logoUrl = computed(() => {
-//   return `layout/images/${layoutConfig.darkTheme.value ? 'logo-white' : 'logo-dark'}.svg`
-// })
+const logoUrl = computed(() => {
+  return `layout/images/${layoutConfig.darkTheme.value ? 'logo-white' : 'logo-dark'}.svg`
+})
 
-// const onTopBarMenuButton = () => {
-//   topbarMenuActive.value = !topbarMenuActive.value
-// }
-// const onSettingsClick = () => {
-//   topbarMenuActive.value = false
-//   router.push('/documentation')
-// }
-// const topbarMenuClasses = computed(() => {
-//   return {
-//     'layout-topbar-menu-mobile-active': topbarMenuActive.value
-//   }
-// })
+const onTopBarMenuButton = () => {
+  topbarMenuActive.value = !topbarMenuActive.value
+}
+const onSettingsClick = () => {
+  topbarMenuActive.value = false
+  router.push('/documentation')
+}
+const topbarMenuClasses = computed(() => {
+  return {
+    'layout-topbar-menu-mobile-active': topbarMenuActive.value
+  }
+})
 
-// const bindOutsideClickListener = () => {
-//   if (!outsideClickListener.value) {
-//     outsideClickListener.value = (event) => {
-//       if (isOutsideClicked(event)) {
-//         topbarMenuActive.value = false
-//       }
-//     }
-//     document.addEventListener('click', outsideClickListener.value)
-//   }
-// }
-// const unbindOutsideClickListener = () => {
-//   if (outsideClickListener.value) {
-//     document.removeEventListener('click', outsideClickListener)
-//     outsideClickListener.value = null
-//   }
-// }
-// const isOutsideClicked = (event) => {
-//   if (!topbarMenuActive.value) return
+const bindOutsideClickListener = () => {
+  if (!outsideClickListener.value) {
+    outsideClickListener.value = (event) => {
+      if (isOutsideClicked(event)) {
+        topbarMenuActive.value = false
+      }
+    }
+    document.addEventListener('click', outsideClickListener.value)
+  }
+}
+const unbindOutsideClickListener = () => {
+  if (outsideClickListener.value) {
+    document.removeEventListener('click', outsideClickListener)
+    outsideClickListener.value = null
+  }
+}
+const isOutsideClicked = (event) => {
+  if (!topbarMenuActive.value) return
 
-//   const sidebarEl = document.querySelector('.layout-topbar-menu')
-//   const topbarEl = document.querySelector('.layout-topbar-menu-button')
+  const sidebarEl = document.querySelector('.layout-topbar-menu')
+  const topbarEl = document.querySelector('.layout-topbar-menu-button')
 
-//   return !(
-//     sidebarEl.isSameNode(event.target) ||
-//     sidebarEl.contains(event.target) ||
-//     topbarEl.isSameNode(event.target) ||
-//     topbarEl.contains(event.target)
-//   )
-// }
+  return !(
+    sidebarEl.isSameNode(event.target) ||
+    sidebarEl.contains(event.target) ||
+    topbarEl.isSameNode(event.target) ||
+    topbarEl.contains(event.target)
+  )
+}
 </script>
 
 <template>
@@ -76,14 +77,14 @@ import { useRouter } from 'vue-router'
       <i class="pi pi-bars"></i>
     </button>
 
-    <!-- <button
+    <button
       class="p-link layout-topbar-menu-button layout-topbar-button"
       @click="onTopBarMenuButton()"
     >
       <i class="pi pi-ellipsis-v"></i>
-    </button> -->
+    </button>
 
-    <!-- <div class="layout-topbar-menu" :class="topbarMenuClasses">
+    <div class="layout-topbar-menu" :class="topbarMenuClasses">
       <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
         <i class="pi pi-calendar"></i>
         <span>Calendar</span>
@@ -96,7 +97,7 @@ import { useRouter } from 'vue-router'
         <i class="pi pi-cog"></i>
         <span>Settings</span>
       </button>
-    </div> -->
+    </div>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './assets/main.css'
 import '@/assets/styles.scss'
 
 import { createApp } from 'vue'
@@ -6,10 +5,19 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import InputText from 'primevue/inputtext'
+import Password from 'primevue/password'
+import Button from 'primevue/button'
+import Checkbox from 'primevue/checkbox'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// app.component('Password', Password);
+// app.component('Button', Button);
+// app.component('InputText', InputText);
+// app.component('Checkbox', Checkbox);
 
 app.mount('#app')
