8969a8ab36bb47d74f2a3d98bce7787525db8490
moises serrano <moycs777@gmail.com>
2023-10-09 10:50:20 -0300

    feat: new component

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/place',
+      name: 'place',
+      component: () => import('../views/PlaceHolder.vue')
     }
   ]
 })
